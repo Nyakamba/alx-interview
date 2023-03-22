752917ffdfaa0a44fcbc2f687a78af5384eaebaf
Nyakamba <omwegaeonkc@gmail.com>
2023-03-22 12:33:35 +0300

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -17,7 +17,9 @@ def makeChange(coins, total):
     coins = sorted(coins)[::-1]
     for coin in coins:
         while coin <= total:
+
             total -= coin
+            
             change += 1
         if (total == 0):
             return change
